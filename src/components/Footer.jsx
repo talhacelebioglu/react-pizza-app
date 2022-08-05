89c6fb0b8898dfa388119038a45a3885e1a59240
talhacelebioglu <104030358+talhacelebioglu@users.noreply.github.com>
2022-08-05 17:55:03 +0300

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,53 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        
+      <div className="footer-above container">
+        <div className="popular-pizza">
+          <strong>Popular Pizza</strong>
+          <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+        <div className="services">
+          <strong>Services</strong>
+          <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+        <div className="customer-information">
+          <strong>Customer Information</strong>
+          <ul>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit.</li>
+          </ul>
+        </div>
+        <div className="contact-us">
+          <strong>Contact Us</strong>
+          <ul>
+            <li>Address: 8901 Pizza Road,Flatlands,D46 85GR</li>
+            <li>Telephone: +1 885 256 894</li>
+            <li>E-mail: mail@pizzatime.org</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-below">
+        <div style={{ color: "rgba(255, 255, 255, 0.4)" }}>
+          <p>Copyright &copy; 2015. Pizza Time </p>
+        </div>
+        <div className="social">
+          <i class="fa-brands fa-facebook fa-2x"></i>
+          <i class="fa-brands fa-instagram fa-2x"></i>
+          <i class="fa-brands fa-twitter fa-2x"></i>
+          <i class="fa-brands fa-youtube fa-2x"></i>
+        </div>
       </div>
     </footer>
   );
